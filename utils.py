39e1f753971c6ac52c193a435fb3769940b2ded8
Brandon Duffany <brandonduffany@gmail.com>
2020-04-04 17:31:42 -0400

    Add test

--- utils.py
+++ utils.py
@@ -1,13 +1,16 @@
 """Miscellaneous utility functions."""
 
 import os
+import shlex
 import subprocess
+import time
 from contextlib import contextmanager
 
 
 def run(command: str, **kwargs):
     """Runs a shell command."""
-    subprocess.run(command.split(), **kwargs, shell=True)
+    print(f'Running command: {command}')
+    subprocess.run(shlex.split(command), **kwargs, shell=True)
 
 
 @contextmanager
@@ -19,3 +22,18 @@ def working_directory(directory: str):
         yield
     finally:
         os.chdir(return_path)
+
+def retry(f):
+    def wrapper(*args, **kwargs):
+        attempts_remaining = 5
+        while True:
+            try:
+                attempts_remaining -= 1
+                return f(*args, **kwargs)
+            except:
+                if attempts_remaining == 0:
+                    raise
+                time.sleep(2)
+
+    return wrapper
+            
